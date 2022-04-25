60186ee4c17bfb5b722e35b4a21043c596f88296
Leon <51866165+Niemand-kun@users.noreply.github.com>
2022-04-25 14:26:03 +0200

    Sample Linked List Node

--- Code/main.c
+++ Code/main.c
@@ -3,5 +3,11 @@
 int main() {
     printf("Hello, World!\n");
     //code for linked lists
+    //Sample Linked List Node
+    struct node
+    {
+        int data;
+        struct node *next;
+    };
     return 0;
 }
